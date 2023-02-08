ea0997d2095c6de422cdb73c1069ff42c9bf27bb
osacrMesh <okerekeifeanyi65@gmail.com>
2023-02-08 09:21:40 +0100

    oscar

---
+++ src/composables/colorPicker.js
@@ -0,0 +1,26 @@
+import { ref, reactive } from "@vue/reactivity";
+import { onMounted, watch, onBeforeMount, watchEffect } from "vue";
+
+const colorManager = () => {
+  const colors = ["red", "green", "blue", "yellow"];
+
+  let message = ref("Pick a color...");
+
+  const addColor = (value) => {
+    const randonNumber = Math.floor(Math.random() * 3) + 1;
+
+    if (colors[randonNumber] === value) {
+      message.value = `You win... [answer: ${colors[randonNumber]}]`;
+      return;
+    }
+    message.value = `Color added successfully [answer: ${colors[randonNumber]}]]`;
+  };
+
+  return {
+    colors,
+    addColor,
+    message,
+  };
+};
+
+export default colorManager;
